32f163ee37fccb022ffc8f9b8922a15b74d474ce
Nicholas Wang <>
2021-06-03 02:28:51 +0000

    Updated Error 404 & 500 Page

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ mail = Mail(app)
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
-
+@app.errorhandler(500)
+def page_not_found1(e):
+	return render_template('500.html'), 500
 
 @app.route('/cookies')
 def cookies():
